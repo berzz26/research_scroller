f546af425f2c047817d29cdebf05e566a212779c
berzz26 <aumtamboli2233@gmail.com>
2025-02-16 11:27:29 +0530

    analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
+
 const geistMono = localFont({
   src: "./fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main className="app-container">{children}</main>
+        <Analytics />
       </body>
     </html>
   );
